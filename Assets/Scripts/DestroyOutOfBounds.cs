044c4adeb8fe85cb998f71dd4e29cf0b2671965d
Ryual <rual37@gmail.com>
2022-02-03 19:25:32 +0200

    Prototype 2 finished before challenge

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+
+    private float topBoundary = 30;
+    private float lowerBoundary = -10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Destroy objects out of players view
+        if (transform.position.z > topBoundary)
+        {
+            Destroy(gameObject);
+        }  
+        else if(transform.position.z < lowerBoundary) 
+        {
+            Debug.Log("Game over!");
+            Destroy(gameObject);
+        }
+    }
+}
